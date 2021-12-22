cbadd6cd2bae2e9c6e74d84fdbc8e39cfb0189bd
B21 <birgulgencustuner@gmail.com>
2021-12-22 14:41:31 +0000

    FeatureB work 3

--- src/main/java/Testfile1jaV.java
+++ src/main/java/Testfile1jaV.java
@@ -2,5 +2,7 @@ public class Testfile1jaV {
     public static void main(String[] args) {
         System.out.println("java is active now");
         System.out.println("java is not active now");
+        int x=8;
+
     }
 }
