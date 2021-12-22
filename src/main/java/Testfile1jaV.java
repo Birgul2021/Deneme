8d730982b788aa8192931bf64c3b2d9daf31659a
B21 <birgulgencustuner@gmail.com>
2021-12-22 14:10:10 +0000

    new java is not ready yet

--- src/main/java/Testfile1jaV.java
+++ src/main/java/Testfile1jaV.java
@@ -1,5 +1,6 @@
 public class Testfile1jaV {
     public static void main(String[] args) {
         System.out.println("java is active now");
+        System.out.println("java is not active now");
     }
 }
